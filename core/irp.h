7679316b6723cdb252740582611754882d914429
George Klees <george.t.klees@gmail.com>
2015-04-29 22:47:23 -0400

    Add IRP request header

--- core/irp.h
+++ core/irp.h
@@ -14,4 +14,17 @@ typedef struct irp
 	int stack_pos;
 } irp_t;
 
+/* Request within an IRP */
+typedef struct req
+{
+	/* Request size */
+	size_t length;
+
+	/* Interface and request code */
+	int iface, request;
+
+	/* Handler state */
+	void *state;
+} req_t;
+
 #endif
